01ff96efc6928499e37c19e9d102d8ff3035086a
learnerChick <sldhana@ymail.com>
2014-04-21 21:44:11 -0700

    Updated

--- Arrays/NextLargerNumber.js
+++ Arrays/NextLargerNumber.js
@@ -6,8 +6,8 @@
  *      3) If left is smaller than right, this will be the first pivot point.
  *      4) Divide the numbers into 2 chunks.  One from index 0 to right before pivot.  The other from pivot to end.
  *      5) Sort second chunk into order.  
- 		6) Find the smallest number larger than the pivot.
- 		7) Go through second part to push into part3 array.  If second part index is equal to pivotReplacer, bypass.	
+ 		6) Go through second part and find number greater than pivot.  If not number, add to part2Swapped.
+ 		   Else	indicate pivotReplacerFound and don't go find the next larger number.
 		8) Join part1, pivotReplacer and part3
 
  *      Algorithm below runs at 0(N^2).
@@ -21,9 +21,9 @@ function findNextLargerNumber(number){
 		var left = number[i-1];
 		var right = number[i];
 		var pivot = -1;
-		var pivotIndex = -1;
 		var pivotReplacer = -1;
 		var part2Swapped = [];
+		var replacerNotFound = false;
 
 		//2) Compare digit on left with right.  If left is larger than right, move on.
 	    //3) If left is smaller than right, this will be the first pivot point.
@@ -40,21 +40,17 @@ function findNextLargerNumber(number){
 			
 			//6) Find the smallest number larger than the pivot.
 			for(var j = 0; j < part2.length; j++){
-				if(part2[j] > pivot){
+				if(part2[j] > pivot && !replacerNotFound){
 					pivotIndex = j;	
 					pivotReplacer = part2[j];
-					break;
+					replacerNotFound = true;
 				}
-			}
-
-			//7) Go through second part to push into part3 array.  If second part index is equal to pivotReplacer, bypass.
-			for(var j = 0; j < part2.length; j++){
-				if(j !== pivotIndex){
+				else{
 					part2Swapped.push(part2[j]);
 				}
 			}
-			
-			//8) Join part1, pivotReplacer and part3
+
+			//8) Join part1, pivotReplacer and part2Swapped
 			return part1.join("") + pivotReplacer + part2Swapped.join("");
 		}
 	}
