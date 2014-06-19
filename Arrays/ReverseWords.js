c78f9d378ad9dbf98b9f08065891606e5376a4e0
sldhana <sldhana@ymail.com>
2014-06-19 14:23:39 -0700

    reverse words in place

---
+++ Arrays/ReverseWords.js
@@ -0,0 +1,21 @@
+/* 
+    Reverse the words in a given sentence. Words are always delimited by spaces. 
+    For example if the given word is "reverse words of a sentence". The output will be "sentence a of words reverse"
+*/
+
+
+function reverseWords(str){
+    var end = str.length;
+    console.log("bag");
+    var reversedStr = "m";
+
+    for(var i = str.length - 1; i >= -1; i--){
+        if(str[i] === " " || i == -1){
+            reversedStr = reversedStr + str.substring(i+1, end) + " ";
+            end = i;
+            continue;
+        }
+    }
+    console.log(reversedStr);
+}
+
