6f83aa2d28e0d1f2064cfce7c9c22fbba2ed5978
sldhana <sldhana@ymail.com>
2014-07-28 21:24:37 -0700

    graph files

---
+++ Graphs/UndirectedGraph.js
@@ -0,0 +1,50 @@
+define(function(){
+    var Graph = function(v){
+        this.vertices = v;
+        this.edges = 0;
+        this.adj = {};
+        this.marked = [];
+
+        for(var i = 0; i < v; i++){
+            this.marked[i] = false;
+        }
+    };
+
+    Graph.prototype.addEdge = function(v, w){
+        if(typeof this.adj[v] === "undefined"){
+            this.adj[v] = [];
+        }
+
+        if(typeof this.adj[w] === "undefined"){
+            this.adj[w] = [];
+        }
+
+        this.adj[v].push(w);
+        this.adj[w].push(v);
+        this.edges++;
+    }
+
+    Graph.prototype.showGraph = function(){
+        for(var i in this.adj){
+            for(var j = 0; j < this.adj[i].length; j++){
+                console.log(i, "-->", this.adj[i][j])
+            }
+        }
+    }
+
+    Graph.prototype.dfs = function(vertex){
+        this.marked[vertex] = true;
+        var self = this;
+        console.log("Visited vertex: ", vertex);
+
+        if(typeof this.adj[vertex] !== "undefined"){
+            this.adj[vertex].forEach(function(ver){
+                if(!self.marked[ver]){
+                    self.dfs(ver);
+                }
+            });
+        }
+    }
+
+    return Graph;
+});
---
+++ Graphs/Vertex.js
@@ -0,0 +1,8 @@
+define(function(){
+    var Vertex = function(label){
+        this.label = label;
+        this.visited = false;
+    }
+
+    return Vertex;
+});
